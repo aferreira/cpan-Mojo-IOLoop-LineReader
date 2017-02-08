6007740422c26da64181d8fa5eb104f13326249e
Adriano Ferreira <a.r.ferreira@gmail.com>
2017-02-07 16:47:16 -0800

    Script with example

---
+++ examples/echo.pl
@@ -0,0 +1,23 @@
+
+use Mojo::Base -strict;
+use Mojo::IOLoop::LineReader;
+
+use Data::Dump 'pp';
+
+my $fn = shift or die "Usage: $0 <filename>\n";
+open my $fh, '<', $fn or die qq{Can't open "$fn": $!};
+
+my $r = Mojo::IOLoop::LineReader->new($fh);
+
+my $c = 0;
+$r->on(
+  readln => sub {
+    $c++;
+    my ($r, $ln) = @_;
+    print "$c: $ln";
+  }
+);
+$r->on(close => sub { say "Bye." });
+
+$r->start;
+$r->reactor->start;
